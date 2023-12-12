332c2ddb4fc4ada98c82028156ab7ccb7038c34f
bdg-cegeplimoilou <Benoit.Gouthiere@etudiant.cegeplimoilou.ca>
2023-12-12 11:17:20 -0500

    Premier TestUnit

--- ModernRecrut.Postulation.API.UnitTests/Services/GenererEvalutationServiceTest.cs
+++ ModernRecrut.Postulation.API.UnitTests/Services/GenererEvalutationServiceTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using FluentAssertions;
+using ModernRecrut.Postulation.API.Interfaces;
+using ModernRecrut.Postulation.API.Models;
+using ModernRecrut.Postulation.API.Services;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +17,22 @@ namespace ModernRecrut.Postulation.API.UnitTests.Services
         public async Task GenererEvaluation_PretentionSalarial15000_Retourne_NoteNoteDetailSalaireInferieur()
         {
             // Etant donné
+            decimal pretentionSalariale = 15000m;
+
+            //var mockGenererEvaluationService = new Mock<IGenererEvaluationService>();
+
+            GenererEvaluationService genererEvaluationService = new GenererEvaluationService();
 
             // Lorsque
+            var note = genererEvaluationService.GenererEvaluation(pretentionSalariale);
 
+            // Alors
+
+            // Verifier Note est
+            note.Should().NotBeNull();
+            note.Should().BeOfType(typeof(Note));
+            note.NomEmeteur.Should().Be("ApplicationPostulation");
+            note.NoteDetail.Should().Be("Salaire inférieur à la norme");
         }
     }
 }
