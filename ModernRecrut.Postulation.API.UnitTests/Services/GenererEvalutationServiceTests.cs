a7cddf1ec312d4fcac0af35753a049000fbd77fc
bdg-cegeplimoilou <Benoit.Gouthiere@etudiant.cegeplimoilou.ca>
2023-12-13 03:29:20 -0500

    GenererEvalutationServiceTests Done

--- ModernRecrut.Postulation.API.UnitTests/Services/GenererEvalutationServiceTests.cs
+++ ModernRecrut.Postulation.API.UnitTests/Services/GenererEvalutationServiceTests.cs
@@ -14,12 +14,10 @@ namespace ModernRecrut.Postulation.API.UnitTests.Services
     public class GenererEvalutationServiceTests
     {
         [Fact]
-        public async Task GenererEvaluation_PretentionSalarial15000_Retourne_NoteNoteDetailSalaireInferieur()
+        public void GenererEvaluation_PretentionSalarialeInferieurNorme_Retourne_NoteNoteDetailSalaireInferieur()
         {
             // Etant donné
             decimal pretentionSalariale = 15000m;
-
-            //var mockGenererEvaluationService = new Mock<IGenererEvaluationService>();
 
             GenererEvaluationService genererEvaluationService = new GenererEvaluationService();
 
@@ -27,17 +25,82 @@ namespace ModernRecrut.Postulation.API.UnitTests.Services
             var note = genererEvaluationService.GenererEvaluation(pretentionSalariale);
 
             // Alors
-
-            // Verifier Note est
             note.Should().NotBeNull();
             note.Should().BeOfType(typeof(Note));
             note.NomEmeteur.Should().Be("ApplicationPostulation");
             note.NoteDetail.Should().Be("Salaire inférieur à la norme");
         }
 
-        // Test entre 20.000 et 39.999 - TODO 
-        // Test entre 40.000 et 79.999 - TODO
-        // Test entre 80.000 et 99.999 - TODO
-        // Test suprérieur à 100.000 - TODO
+        [Fact]
+        public void GenererEvaluation_PretentionSalarialeProcheInferieurNorme_Retourne_NoteNoteDetailSalaireProcheInferieur()
+        {
+            // Etant donné
+            decimal pretentionSalariale = 25000m;
+
+            GenererEvaluationService genererEvaluationService = new GenererEvaluationService();
+
+            // Lorsque
+            var note = genererEvaluationService.GenererEvaluation(pretentionSalariale);
+
+            // Alors
+            note.Should().NotBeNull();
+            note.Should().BeOfType(typeof(Note));
+            note.NomEmeteur.Should().Be("ApplicationPostulation");
+            note.NoteDetail.Should().Be("Salaire proche mais inférieur à la norme");
+        }
+
+        [Fact]
+        public void GenererEvaluation_PretentionSalarialeDansNorme_Retourne_NoteNoteDetailSalaireNorme()
+        {
+            // Etant donné
+            decimal pretentionSalariale = 55000m;
+
+            GenererEvaluationService genererEvaluationService = new GenererEvaluationService();
+
+            // Lorsque
+            var note = genererEvaluationService.GenererEvaluation(pretentionSalariale);
+
+            // Alors
+            note.Should().NotBeNull();
+            note.Should().BeOfType(typeof(Note));
+            note.NomEmeteur.Should().Be("ApplicationPostulation");
+            note.NoteDetail.Should().Be("Salaire dans la norme");
+        }
+
+        [Fact]
+        public void GenererEvaluation_PretentionSalarialeProcheSuperieurNorme_Retourne_NoteNoteDetailSalaireProcheSuperieur()
+        {
+            // Etant donné
+            decimal pretentionSalariale = 85000m;
+
+            GenererEvaluationService genererEvaluationService = new GenererEvaluationService();
+
+            // Lorsque
+            var note = genererEvaluationService.GenererEvaluation(pretentionSalariale);
+
+            // Alors
+            note.Should().NotBeNull();
+            note.Should().BeOfType(typeof(Note));
+            note.NomEmeteur.Should().Be("ApplicationPostulation");
+            note.NoteDetail.Should().Be("Salaire proche mais supérieur à la norme");
+        }
+
+        [Fact]
+        public void GenererEvaluation_PretentionSalarialeSuperieurNorme_Retourne_NoteNoteDetailSalaireSuperieur()
+        {
+            // Etant donné
+            decimal pretentionSalariale = 125000m;
+
+            GenererEvaluationService genererEvaluationService = new GenererEvaluationService();
+
+            // Lorsque
+            var note = genererEvaluationService.GenererEvaluation(pretentionSalariale);
+
+            // Alors
+            note.Should().NotBeNull();
+            note.Should().BeOfType(typeof(Note));
+            note.NomEmeteur.Should().Be("ApplicationPostulation");
+            note.NoteDetail.Should().Be("Salaire supérieur à la norme");
+        }
     }
 }
